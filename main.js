3149c062fab5561337ef1a865e68f492257e8e5a
Ziad Hosny <ziadhosny007#gmail.com>
2022-06-30 19:31:04 +0200

    update again

--- main.js
+++ main.js
@@ -97,11 +97,12 @@ function flipCard(e) {
 
   if (firstFlip != secondFilp && secondFilp > -1) {
     setTimeout(() => {
+      alert('dsf');
       card.style = "backgroundImage : url('images/card.png')";
       firstCard.style = "backgroundImage : url('images/card.png')";
       firstFlip = -1;
       secondFilp = -2;
-    }, 300);
+    }, 700);
   }
   if (firstFlip == secondFilp) {
     itemsTaken[items.indexOf(firstCard)] = 'Z';
